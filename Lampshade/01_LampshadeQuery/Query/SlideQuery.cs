5cc5d332913403b39288e7e2a9a9fe25011ee820
Sepanta Noroozian <sepantanoroozian@gmail.com>
2021-11-29 11:15:53 +0330

    Slider in UI Implemented

---
+++ Lampshade/01_LampshadeQuery/Query/SlideQuery.cs
@@ -0,0 +1,36 @@
+﻿using _01_LampshadeQuery.Contracts;
+using _01_LampshadeQuery.Contracts.Slide;
+using ShopManagement.Infrastructure.EFCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LampshadeQuery.Query
+{
+    public class SlideQuery : ISlideQeury
+    {
+        private readonly ShopContext _shopContext;
+
+        public SlideQuery(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+        }
+
+        public List<SlideQueryModel> GetSlides()
+        {
+            return _shopContext.Slides.Where(p => p.IsRemoved == false).Select(p => new SlideQueryModel
+            {
+                Picture = p.Picture,
+                PictureAlt = p.PictureAlt,
+                PictureTitle = p.PictureTitle,
+                BtnText = p.BtnText,
+                Heading = p.Heading,
+                Link = p.Link,
+                Text = p.Text,
+                Title = p.Title
+            }).ToList();
+        }
+    }
+}
---
+++ Lampshade/ServiceHost/ViewComponents/SlideViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using _01_LampshadeQuery.Contracts.Slide;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceHost.ViewComponents
+{
+    public class SlideViewComponent : ViewComponent
+    {
+        private readonly ISlideQeury _slideQeury;
+
+        public SlideViewComponent(ISlideQeury slideQeury)
+        {
+            _slideQeury = slideQeury;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var slides = _slideQeury.GetSlides();
+            return View(slides);
+        }
+    }
+}

5ff2030cc7676009623c9950e683f1e1190f434e
Sepanta Noroozian <sepantanoroozian@gmail.com>
2021-11-30 12:55:31 +0330

    Another Decrease Method Added to IInventoryApplication

--- Lampshade/InventoryManagement.Application.Contracts/Inventory/IInventoryApplication.cs
+++ Lampshade/InventoryManagement.Application.Contracts/Inventory/IInventoryApplication.cs
@@ -12,6 +12,7 @@ namespace InventoryManagement.Application.Contracts.Inventory
         OperationResult Create(CreateInventory command);
         OperationResult Edit(EditInventory command);
         OperationResult Increase(IncreaseInventory command);
+        OperationResult Decrease(DecreaseInventory command);
         OperationResult Decrease(List<DecreaseInventory> command);
         EditInventory GetDetails(long id);
         List<InventoryViewModel> Search(InventorySearchModel searchModel);
